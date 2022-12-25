c01d9140c21f4c31fd47996e2271f4ccaab7c109
Dmitry Zuikov <dzuikov@gmail.com>
2022-12-25 07:23:10 +0300

    removed extras

--- flake.nix
+++ flake.nix
@@ -4,16 +4,6 @@ description = "my-news-generator";
 inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     haskell-flake-utils.url = "github:ivanovs-4/haskell-flake-utils";
-
-    binary-bits.url = "github:ivanovs-4/binary-bits";
-    binary-bits.inputs.nixpkgs.follows = "nixpkgs";
-    binary-bits.inputs.haskell-flake-utils.follows = "haskell-flake-utils";
-
-    codec.url = "github:ivanovs-4/codec";
-    codec.inputs.nixpkgs.follows = "nixpkgs";
-    codec.inputs.haskell-flake-utils.follows = "haskell-flake-utils";
-    codec.inputs.binary-bits.follows = "binary-bits";
-
 };
 
 outputs = { self, nixpkgs, haskell-flake-utils, ... }@inputs:
@@ -26,17 +16,12 @@ outputs = { self, nixpkgs, haskell-flake-utils, ... }@inputs:
       shellWithHoogle = true;
 
       haskellFlakes = with inputs; [
-        binary-bits
-        codec
       ];
 
       hpPreOverrides = { pkgs }: new: old:
         with pkgs.haskell.lib;
         with haskell-flake-utils.lib;
         tunePackages pkgs old {
-          mr-env            = [ (jailbreakUnbreak pkgs) dontCheck ];
-          thread-supervisor = [ (jailbreakUnbreak pkgs) dontCheck ];
-          higgledy          = [ (jailbreakUnbreak pkgs) dontCheck ];
         };
 
       packagePostOverrides = { pkgs }: with pkgs; with haskell.lib; [
